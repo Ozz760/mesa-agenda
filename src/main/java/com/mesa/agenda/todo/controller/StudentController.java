e2ac509c281a44695585b79cfc289900c4e13f85
vladkim91 <vkcoding21@gmail.com>
2025-04-10 14:15:47 -0700

    route name changed to 'rest/*'. Post and get requests are 200 through postman

--- src/main/java/com/mesa/agenda/todo/controller/StudentController.java
+++ src/main/java/com/mesa/agenda/todo/controller/StudentController.java
@@ -5,10 +5,11 @@ import com.mesa.agenda.todo.service.StudentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("rest/students")
 public class StudentController {
 
     private final StudentService studentService;
