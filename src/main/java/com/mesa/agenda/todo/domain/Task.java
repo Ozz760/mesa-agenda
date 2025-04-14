a4a6fd72b980cd109f7a92e836bf5db038e673db
Ian <ian.char.phillips@gmail.com>
2025-04-14 16:35:54 -0700

    Task & TaskController WIP -Ian

--- src/main/java/com/mesa/agenda/todo/domain/Task.java
+++ src/main/java/com/mesa/agenda/todo/domain/Task.java
@@ -1,4 +1,65 @@
 package com.mesa.agenda.todo.domain;
+import jakarta.persistence.Entity;
 
+import java.time.LocalDateTime;
+
+@Entity
 public class Task {
+
+    //do we need the same @Id from student? Or is it shared by Class.java? etc.
+    //@Id
+    //@GeneratedValue
+    //private long id;
+
+    private String taskName;
+    private String location;
+    private boolean isComplete;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dueDate;
+
+    //classId foreign key required
+
+    //default constructor for Task
+    public Task() {
+
+    }
+
+    //full constructor
+    //@TODO: still needs @Id and foreign key values added to constructor
+    public Task(String taskName, String location, boolean isComplete, LocalDateTime dateCreated, LocalDateTime dueDate) {
+        this.taskName = taskName;
+        this.location = location;
+        this.isComplete = false;
+        this.dateCreated = dateCreated;
+        this.dueDate = dueDate;
+    }
+
+    //getters and setters
+    //still missing getters/setters for Id and classId FK
+    public String getTaskName() { return taskName; }
+    public void setTaskName(String taskName) { this.taskName = taskName; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public boolean isComplete() { return isComplete; }
+    public void setComplete(boolean complete) { isComplete = complete; }
+
+    public LocalDateTime getDateCreated() { return dateCreated; }
+    public void setDateCreated(LocalDateTime dateCreated) { this.dateCreated = dateCreated; }
+
+    public LocalDateTime getDueDate() { return dueDate; }
+    public void setDueDate(LocalDateTime dueDate) { this.dueDate = dueDate;}
+
+    //@TODO: toString needs to be updated with class variables when they're added
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskName='" + taskName + '\'' +
+                ", location='" + location + '\'' +
+                ", isComplete=" + isComplete +
+                ", dateCreated=" + dateCreated +
+                ", dueDate=" + dueDate +
+                '}';
+    }
 }
